0b2296cd5bf38754121f19f2f50e6a62ea2ab87e
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-02 22:26:45 +0530

    newvm1

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-
 # -----------------------------
 # Resource Group
 # -----------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "rg-linux-vm-rg"
-  location = "eastus"
+  location = "eastus2" # Moved to East US 2 for better capacity
 }
 
 # -----------------------------
@@ -49,12 +48,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "ubuntu-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  zone                = "1" # Adding an explicit zone can help bypass capacity issues
   
-  # Changed size to Standard_D2s_v3 to avoid capacity restrictions
-  size                = "Standard_D2s_v3" 
-  
-  admin_username      = "azureuser"
-  admin_password      = var.admin_password
+  size                            = "Standard_D2s_v3" 
+  admin_username                  = "azureuser"
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
@@ -68,16 +66,16 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy" # Ubuntu 22.04 LTS (often more stable)
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
 }
 
 # -----------------------------
-# Outputs
+# Output
 # -----------------------------
 output "vm_private_ip" {
-  description = "The private IP address of the deployed VM"
+  description = "The private IP address of the VM"
   value       = azurerm_network_interface.nic.private_ip_address
 }
