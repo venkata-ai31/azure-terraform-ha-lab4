3a4cf6c313ce3b96481a4b8eec8e7bc1585728be
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-02 22:02:01 +0530

    vm

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-
-  required_version = ">=1.2.0"
-}
-
-provider "azurerm" {
-  features {}
-}
 
 # -----------------------------
 # Resource Group
