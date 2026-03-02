acddb7b7e9aecce457c2a5d559be7f82290b02b4
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-02 20:41:14 +0530

    vnet

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-day1-terraform"
   location = "East US"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-ha"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "public-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
