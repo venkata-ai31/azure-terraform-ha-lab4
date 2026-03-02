141f74987e28c78801d8cde57bd14c23b1cf277d
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-02 21:54:47 +0530

    vm

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "24_04-lts-gen2"
     version   = "latest"
   }
 }
