e73d6aeb8251fb4d6d519874f050e78d2672dad0
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-03 08:41:41 +0530

    ApacheTest_LB_NSGRules

--- main.tf
+++ main.tf
@@ -152,6 +152,23 @@ resource "azurerm_lb_rule" "http" {
   probe_id                       = azurerm_lb_probe.probe.id
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "web-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 
 # -----------------------------
 # Output
@@ -159,5 +176,9 @@ resource "azurerm_lb_rule" "http" {
 output "vm_private_ip" {
   description = "The private IP address of the VM"
   value       = azurerm_network_interface.nic[0].private_ip_address
+}
+
+output "load_balancer_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
 
